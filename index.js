218d37ed8e95d56920b0900a2b923aee61cd5255
Razor2211 <67188057+Razor2211@users.noreply.github.com>
2020-11-16 16:46:18 +0530

    custom prefix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client,Collection } = require("discord.js")
-
+const mongoose = require("mongoose")
 const fs = require("fs")
 const client = new Client ({ 
     disableEveryone:"true" // This makes sure that the bot does not mention everyone
@@ -13,25 +13,57 @@ const prefix = config.prefix;
 ['command'].forEach(handler=>{
     require(`./handler/${handler}`)(client);
 })
+try{   
+     mongoose.connect(config.mongoPass,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    })
+    const Data = require("./models/guilddata")
 client.on("ready",()=>{
     console.log("Tutorial Bot is online")
 })
 
 client.on("message",async message =>{
-    client.prefix = prefix;
-    if(message.author.bot) return; // This line makes sure that the bot does not respond to other bots
-    if(!message.guild) return;
-    if(!message.content.startsWith(prefix)) return; // This line makes sure that the bot does not respond to other messages with the bots prefix
-    if(!message.member) message.member = await message.guild.fetchMember(message);
+try{
+    if(message.channel.type !=="dm"){
+        Data.findOne({
+            guild:message.guild.id
+        },(err,data) =>{
+            if(err) console.log(err)
+            if(!data){
+                const newData = new Data({
+                    guild:message.guild.id,
+                    guildname:message.guild.name,
+                    guildprefix:"!",
+                })
+                newData.save().catch(err=>console.log(err))
+            }else{
+                try{
+                    let prefix = data.guildprefix;
+                    if(!message.content.startsWith(prefix) || message.author.bot) return
+                    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+                    const cmd = args.shift().toLowerCase()
+                    if(cmd.length ===0) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if(cmd.length ===0) return;
+                    let command = client.commands.get(cmd)
+                    if(command)
+                    if(!command) return
+                    command.run(client,message,args)
+                } catch(err) {console.log(err)}
+            }
+        })
+    }else{
+        let prefix = "!"
+        if(!message.content.startsWith(prefix) || message.author.bot) return
+        const args = message.content.slice(prefix.length).trim().split(/ +/g)
+        const cmd = args.shift().toLowerCase()
+        if(cmd.length ===0) return;
 
-    let command = client.commands.get(cmd);
-    if(!command) command = client.commands.get(client.aliases.get(cmd));
-    if(command)
-    command.run(client,message,args);
-})
-
+        let command = client.commands.get(cmd)
+        if(command)
+            command.run(client,message,args)
+        
+        }
+}catch(err){console.log(err)}
+})}catch(err){console.log(err)}
 client.login("Token")//Enter your bot token here
